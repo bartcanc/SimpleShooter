1f4001650bb2590b9ad58b099df5db31c26c0e8a
bartcanc <bartekr2002@gmail.com>
2025-08-12 15:23:28 +0200

    iterating over actors

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -3,7 +3,8 @@
 
 #include "KillEmAllGameMode.h"
 
-
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
     Super::PawnKilled(PawnKilled);
@@ -12,6 +13,26 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
     APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
     if(PlayerController)
     {
-        PlayerController->GameHasEnded(nullptr, false);
+        EndGame(false);
+    }
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+    for(AController* Controller : TActorRange<AController>(GetWorld()))     // iterating over actors in the world to check if the player is dead
+    {
+        // if((Controller -> IsPlayerController() and bIsPlayerWinner) or      //  if found actor is the player and if the player is not dead
+        // (!Controller -> IsPlayerController() and !bIsPlayerWinner))         //  if found actor is not a player and if the player is dead
+        // {
+        //     Controller->GameHasEnded(nullptr, true);
+        // }
+        // else
+        // {
+        //     Controller->GameHasEnded(nullptr, false);
+        // }
+        Controller -> GameHasEnded(
+            Controller->GetPawn(),
+            Controller -> IsPlayerController() == bIsPlayerWinner
+        );
     }
 }
